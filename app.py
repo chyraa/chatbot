7f7a26f077aaea700500a28a6426b4fb3e22bc3a
chyraa <chindyrahmawati775@gmail.com>
2025-06-11 22:10:38 +0700

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ user_input = st.text_input("Ketik pesan:", "")
 
 if st.button("Kirim"):
     try:
-        response = requests.post("https://chatbot-production-22e9.up.railway.app/", json={"user_input": user_input})
+        response = requests.post("https://chatbot-production-22e9.up.railway.app/get_response", json={"user_input": user_input})
         chatbot_response = response.json().get("response", "Error: No response received")
         st.write("Chatbot:", chatbot_response)
     except requests.exceptions.ConnectionError:
